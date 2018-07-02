011e25084d89f6dad4ceee53e74edb92345457f8
ConstanzaGarcia <o.conii.o@gmail.com>
2018-07-01 20:33:57 -0400

    imprime comentarios y corazon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,38 @@
 const boton = document.getElementById("btn");
 boton.addEventListener("click", () => {
-    //acá guardo el comentario ingresado por el usuario
-    let comments = document.getElementById("tweet").value;
+  // Acá guardo el comentario ingresado por el usuario en el textarea
+  let comments = document.getElementById("tweet").value;
 
-    //limpiar el textarea
-    document.getElementById("tweet").value=" ";
+  // Limpiar el textarea
+  document.getElementById("tweet").value = " ";
 
-    //contenedor donde dejaré mis comentarios en html
-    const tweets= document.getElementById("tweets"); 
+  //contenedor donde dejaré mis comentarios en HTML
+  const cont = document.getElementById("tweets");
 
-    //crear un div contenedor
-    const newComments = document.createElement("div");
+  //Crear div contenedor
+  const newComments = document.createElement("div");
+
+  //Validar que el textarea tenga un comentario y no esté vacío
+  if (comments.length === 0 || comments === null){
+    alert ("Debes ingresar un mensaje");
+    return false;
+  }
+
+  //Crear checkbox
+  const chck = document.createElement("input");
+  chck.type = "checkbox";
+
+  //Crear ícono corazón
+  const heart =  document.createElement("i");
+  heart.classList.add ('fas', 'fa-heart', 'heart');
+
+  // Nodos de texto del textarea
+  let textNewComment = document.createTextNode(comments);
+
+  const contenedorElemento = document.createElement("p");
+  contenedorElemento.appendChild(textNewComment);
+  newComments.appendChild(heart);
+  newComments.appendChild(contenedorElemento);
+
+  cont.appendChild(newComments);
 })
